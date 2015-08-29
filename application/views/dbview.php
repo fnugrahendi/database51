92164358d8ad6b1fd89afaff2097a930a32ecda3
Ferindra Nugrahendi <fnugrahendi@gmail.com>
2015-08-29 17:38:02 +0700

    bootstrap-ing its style

--- application/views/dbview.php
+++ application/views/dbview.php
@@ -1,5 +1,5 @@
 		<div class="content">
-			<h1>Database Tipuan</h1>
+			<h1 id="judul">Database Tipuan</h1>
 			<?php
 			if($dbs!=null){
 				$i=0;
@@ -21,7 +21,11 @@
 					<p>Maafkan kami yang gagal menemukan datanya</p>
 				</div>
 			<?php } ?>
-			<p><a href="<?php echo site_url('database/'); ?>"><< Back</a></p>
+			<p>
+				<a href="<?php echo site_url('database/'); ?>">
+					<button type="button" class="btn btn-default">Back</button>
+				</a>
+			</p>
 		</div>			
 	</body>
 </html>
